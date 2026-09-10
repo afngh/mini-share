a98b3a2d074f09fbd60295854da18aa27ff3ebc4
afngh <shaikafnan095@gmail.com>
2026-09-10 15:41:12 +0530

    added code for streaming

--- streaming/FileTransferService.java
+++ streaming/FileTransferService.java
@@ -1,6 +1,27 @@
+package streaming;
 
-// Remote Interface for RMI File Transfer Service
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ * Remote Interface for Java RMI File Transfer Service.
+ * Defines remote methods for room management and file streaming.
+ */
 public interface FileTransferService extends Remote {
-    // Remote method declarations:
-    // createRoom, joinRoom, uploadFile, downloadFile
+
+    // Room Management Remote Methods
+    String createRoom() throws RemoteException;
+    void createRoom(String roomCode) throws RemoteException;
+
+    boolean joinRoom(String roomCode, String username) throws RemoteException;
+    boolean joinRoom(String roomCode) throws RemoteException;
+
+    // File Streaming Remote Methods
+    void sendFile(String roomCode, String fileName, byte[] fileData) throws RemoteException;
+    byte[] receiveFile(String roomCode) throws RemoteException;
+    String getLatestFileName(String roomCode) throws RemoteException;
+
+    // Direct Upload / Download Aliases
+    void uploadFile(String roomCode, byte[] fileData, String fileName) throws RemoteException;
+    byte[] downloadFile(String roomCode) throws RemoteException;
 }
